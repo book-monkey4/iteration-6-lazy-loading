d7c806586356247b706b746018a2c930b547322e
The Buildbot <buildbot@angular2buch.de>
2020-03-04 22:41:47 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-src/commit/116088f0615a9a5f8835653706a8733338376001
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-src/47

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
